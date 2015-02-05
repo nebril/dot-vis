093becccccfe26bb38a9cdfda5e0e0c4e5fd66cf
Maciej Kwiek <mkwiek@mirantis.com>
2015-02-05 17:27:39 +0100

    Refactored parser

--- back/dot2jsoncyto.py
+++ back/dot2jsoncyto.py
@@ -1,65 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import os
-import json
-import sys
 
-import pydot
+from dot2jsonparser import Dot2JSONParser
 
+argparser = argparse.ArgumentParser()
+argparser.add_argument('source')
+argparser.add_argument('target')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('source')
+args = argparser.parse_args()
 
-args = parser.parse_args()
+parser = Dot2JSONParser(args.source, args.target)
 
-graph = pydot.graph_from_dot_file(args.source)
-
-
-nodes = []
-edges = []
-for node in graph.get_node_list():
-    try:
-        function, percentage, percentage2, times = tuple(
-            node.get_label().split('\\n'))
-        nodes.append({
-            "data": {
-                "id": str(int(node.get_name())),
-                "funcName": function,
-                "percentage": float(percentage.strip('()%')),
-                "percentage2": float(percentage2.strip('()%')),
-                "callCount": times,
-                "label": "{0}, {1}, {2}".format(function, percentage, times),
-            },
-            "position": {
-                "x": 0,
-                "y": 0,
-            },
-        })
-        
-    except (ValueError, AttributeError) as e:
-        sys.stderr.write("Skipped {0}".format(node.get_name()));
-        pass
-
-for edge in graph.get_edge_list():
-    percentage, times = tuple(
-        edge.get_label().strip('"').split('\xc3')[0].split('\\n')
-    )
-    edges.append({
-        "data": {
-            "id": 'e' + str(int(edge.get_source())) + '_' +  str(int(edge.get_destination())),
-            "source": edge.get_source(),
-            "target": edge.get_destination(),
-            "percentage": float(percentage.strip('()%')),
-            "callCount" : int(times),
-            "label": "{0}, {1}x".format(percentage, times),
-
-        }
-    })
-
-print json.dumps(
-  {
-    "nodes": nodes,
-    "edges": edges,
-  }
-)
+print parser.parse()
